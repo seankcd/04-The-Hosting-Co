43441dac4fb09f7473839d98389426f0fbab4585
Sean Courtney <seankc@me.com>
2020-11-27 23:13:14 -0800

    first commit

---
+++ dist/js/observers.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector("#main");
+
+const sectionOneOptions = {
+  rootMargin: "-500px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
