00d8d74f104cb67386029f944ade758745bbf46c
Sean Courtney <seankc@me.com>
2020-11-30 19:00:46 -0800

    Nav fixes and hero section

--- dist/js/observers.js
+++ dist/js/observers.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 const sectionOne = document.querySelector("#main");
 
 const sectionOneOptions = {
-  rootMargin: "-500px 0px 0px 0px"
+  rootMargin: "-600px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(
